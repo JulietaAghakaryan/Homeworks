04ae13820c21de70632bf5646a15dbc39c74be36
Julieta Aghakaryan <julieta_aghakaryan@edu.aua.am>
2021-09-19 22:10:43 +0400

    Homework

--- src/Week7/ex12.java
+++ src/Week7/ex12.java
@@ -6,38 +6,48 @@ import java.util.Random;
 
 public class ex12 {
 
+    public static void main(String[] args) {
+        System.out.println(makeAList(5, "double"));
+        System.out.println(randomDouble(1, "double"));
+    }
 
-    public static List<Number> makeAList(int number, Number type){
+    public static List<Number> makeAList(int number, String type){
         List<Number> list=new ArrayList<>();
-        if(type instanceof Integer || type instanceof Long  || type instanceof Short){
+        if("int".equals(type) || "long".equals(type) || "short".equals(type)){
             for (int i = 0; i < number; i++) {
                 list.add(i*2);
             }
         }
-        else if(type instanceof Double || type instanceof Float){
+        else if("double".equals(type) || "float".equals(type)){
             for (double i = 0; i < number; i++) {
                 list.add(i*2);
             }
         }
-        else if(type instanceof Byte){
+        else if("byte".equals(type)){
             if(number>-128 && number<=127){
                 for (int i = 0; i < number; i++) {
                     list.add(i*2);
                 }
             }
         }
+        else{
+            throw new IllegalArgumentException();
+        }
         return list;
 
     }
 
-    public static List<Double> randomDouble(int number, Number type){
+    public static List<Double> randomDouble(int number, String  type){
         Random r= new Random();
         List<Double> list=new ArrayList<>();
-        if(type instanceof Double) {
+        if("double".equals(type)) {
 
             for (int i = 0; i < number; i++) {
                 list.add(r.nextDouble()*2);
             }
+        }
+        else{
+            throw new IllegalArgumentException("Type is not double");
         }
         return list;
     }
