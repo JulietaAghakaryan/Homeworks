4d29681bbdbfd8b874276707372ddb0596b82da7
Julieta Aghakaryan <julieta_aghakaryan@edu.aua.am>
2021-09-19 21:56:24 +0400

    Homework

--- src/Week7/ex12.java
+++ src/Week7/ex12.java
@@ -1,7 +1,44 @@
 package Week7;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ex12 {
 
+
+    public static List<Number> makeAList(int number, Number type){
+        List<Number> list=new ArrayList<>();
+        if(type instanceof Integer || type instanceof Long  || type instanceof Short){
+            for (int i = 0; i < number; i++) {
+                list.add(i*2);
+            }
+        }
+        else if(type instanceof Double || type instanceof Float){
+            for (double i = 0; i < number; i++) {
+                list.add(i*2);
+            }
+        }
+        else if(type instanceof Byte){
+            if(number>-128 && number<=127){
+                for (int i = 0; i < number; i++) {
+                    list.add(i*2);
+                }
+            }
+        }
+        return list;
+
+    }
+
+    public static List<Double> randomDouble(int number, Number type){
+        Random r= new Random();
+        List<Double> list=new ArrayList<>();
+        if(type instanceof Double) {
+
+            for (int i = 0; i < number; i++) {
+                list.add(r.nextDouble()*2);
+            }
+        }
+        return list;
+    }
 }
